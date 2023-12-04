77d50b4f6f62758947a21d9c52e9e4b99ed5c516
Justin Chen <jchen4086@gmail.com>
2023-12-03 22:43:26 -0500

    halt operation

--- operations/halt.v
+++ operations/halt.v
@@ -1,1 +1,20 @@
-// TODO add halt operation
+// output halt_program signals when program has ended
+// check this value in main function
+module halt (
+  input clk,
+  input rst,
+  
+  input wire halt_signal,
+  output reg halt_program
+);
+  always @(posedge clk or posedge rst) begin
+    if (rst) begin
+      halt_signal <= 1'b0; // set to false
+      halt_program <= 1'b0
+    end else begin
+      if (halt_signal) begin
+        halt_program <= 1'b1;
+      end
+    end
+  end
+endmodule
